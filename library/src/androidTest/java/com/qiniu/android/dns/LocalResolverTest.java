acdee578ddcedaad0b0c262e80787d503e79a0c9
longbai <slongbai@gmail.com>
2015-06-24 23:22:07 +0800

    refactor

--- library/src/androidTest/java/com/qiniu/android/dns/LocalResolverTest.java
+++ library/src/androidTest/java/com/qiniu/android/dns/LocalResolverTest.java
@@ -15,8 +15,12 @@ import java.net.UnknownHostException;
  * Created by bailong on 15/6/17.
  */
 public class LocalResolverTest extends AndroidTestCase {
-    public void testLocal(){
-        IResolver resolver = AndroidDnsServer.defaultResolver();
+
+    private  void template(String ip) throws UnknownHostException {
+        Resolver resolver = new Resolver(InetAddress.getByName(ip));
+        template(resolver);
+    }
+    private void template(IResolver resolver){
         try {
             Record[] records = resolver.query(new Domain("baidu.com"));
             Assert.assertNotNull(records);
@@ -24,76 +28,37 @@ public class LocalResolverTest extends AndroidTestCase {
         } catch (IOException e) {
             Assert.fail(e.getMessage());
         }
+    }
+    public void testLocal(){
+        template(AndroidDnsServer.defaultResolver());
     }
 
 //    http://www.alidns.com/
     public void testAli() throws UnknownHostException {
-        Resolver resolver = new Resolver(InetAddress.getByName("223.5.5.5"));
-        try {
-            Record[] records = resolver.query(new Domain("baidu.com"));
-            Assert.assertNotNull(records);
-            Assert.assertTrue(records.length>0);
-        } catch (IOException e) {
-            Assert.fail(e.getMessage());
-        }
+        template("223.5.5.5");
     }
 
 //    https://www.114dns.com/
     public void test114() throws UnknownHostException {
-        Resolver resolver = new Resolver(InetAddress.getByName("114.114.114.114"));
-        try {
-            Record[] records = resolver.query(new Domain("baidu.com"));
-            Assert.assertNotNull(records);
-            Assert.assertTrue(records.length>0);
-        } catch (IOException e) {
-            Assert.fail(e.getMessage());
-        }
+        template("114.114.115.115");
     }
 
 //    http://dudns.baidu.com/
     public void testBaidu() throws UnknownHostException {
-        Resolver resolver = new Resolver(InetAddress.getByName("180.76.76.76"));
-        try {
-            Record[] records = resolver.query(new Domain("baidu.com"));
-            Assert.assertNotNull(records);
-            Assert.assertTrue(records.length>0);
-        } catch (IOException e) {
-            Assert.fail(e.getMessage());
-        }
+        template("180.76.76.76");
     }
 
 //    http://www.sdns.cn/
     public void testCnnic() throws UnknownHostException {
-        Resolver resolver = new Resolver(InetAddress.getByName("1.2.4.8"));
-        try {
-            Record[] records = resolver.query(new Domain("baidu.com"));
-            Assert.assertNotNull(records);
-            Assert.assertTrue(records.length>0);
-        } catch (IOException e) {
-            Assert.fail(e.getMessage());
-        }
+        template("1.2.4.8");
     }
 
     public void testGoogle() throws UnknownHostException {
-        Resolver resolver = new Resolver(InetAddress.getByName("8.8.4.4"));
-        try {
-            Record[] records = resolver.query(new Domain("baidu.com"));
-            Assert.assertNotNull(records);
-            Assert.assertTrue(records.length>0);
-        } catch (IOException e) {
-            Assert.fail(e.getMessage());
-        }
+        template("8.8.4.4");
     }
 
 //    http://www.dnspai.com/
     public void testDnspai() throws UnknownHostException {
-        Resolver resolver = new Resolver(InetAddress.getByName("101.226.4.6"));
-        try {
-            Record[] records = resolver.query(new Domain("baidu.com"));
-            Assert.assertNotNull(records);
-            Assert.assertTrue(records.length>0);
-        } catch (IOException e) {
-            Assert.fail(e.getMessage());
-        }
+        template("101.226.4.6");
     }
 }
